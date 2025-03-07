f8aeebe3498e426b6e5b40008f95abd8836e0c17
kirithns <kirithns2201@gmail.com>
2025-03-07 11:23:28 +0530

    updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+const config = {
   darkMode: "class",
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
